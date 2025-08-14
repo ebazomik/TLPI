17b63cb8e658931d5da0191a15604fb2dd64e96f
ebazomik <kimosabe.mr@gmail.com>
2025-08-14 16:46:09 +0200

    fix sparse file loop logic

--- cp_command/main.c
+++ cp_command/main.c
@@ -4,53 +4,54 @@
 #include <string.h>
 #include <errno.h>
 
-#define BUFF_SIZE 1024
+#define BUFF_SIZE 4096
 
 int main(int argc, char *argv[]){
-    char buffer[BUFF_SIZE + 1];
-    int file_source, file_target, flags, mode, bytes_writted, bytes_readed;
-    
-    if(argc < 3){
+    char buffer[BUFF_SIZE];
+    int file_source, file_target;
+    ssize_t bytes_readed;
+
+    if(argc != 3){
         printf("%s [file-source] [file-target]\n", argv[0]);
-        printf("%s\n", strerror(errno));
         return 1;
     }
     
     file_source = open(argv[1], O_RDONLY);
     if(file_source == -1){
         printf("Error on open %s\n", argv[1]);
-        printf("%s\n", strerror(errno));
         return 1;
     }
 
-    flags = O_RDWR | O_CREAT;
-    mode = S_IRUSR | S_IWUSR;
+    int flags = O_WRONLY | O_CREAT | O_TRUNC;
+    mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
     
     file_target = open(argv[2], flags, mode);
     if(file_target == -1){
         printf("Error on open %s\n", argv[2]);
-        printf("%s", strerror(errno));
         close(file_source);
         return 1;
     }
 
     while( (bytes_readed = read(file_source, buffer, BUFF_SIZE)) > 0 ){
-        buffer[bytes_readed] = '\0';
-        bytes_writted = write(file_target, buffer, bytes_readed);
-        if(bytes_writted != bytes_readed){
-            printf("Error on write or partial write occurred\n");
-            printf("%s\n", strerror(errno));
-            close(file_source);
-            close(file_target);
-            return 1;
-        }
+        int i = 0;
+        while(i < bytes_readed){
+            if(buffer[i] != '\0'){
+                int start_data = i;
+                while(i < bytes_readed && buffer[i] != '\0') i++;
+                int size_data = (i - start_data);
+                write(file_target, &buffer[start_data], size_data);
+            } else {
+                int start_hole = i;
+                while(i < bytes_readed && buffer[i] == '\0') i++;
+                int size_hole = (i - start_hole);
+                lseek(file_target, size_hole, SEEK_CUR);
+            }
+        } 
     }
     
-    int close_source = close(file_source);
-    int close_target = close(file_target);
-    if(close_source != 0 || close_target != 0){
+    if(close(file_source) != 0 || close(file_target) != 0){
         printf("Error on close target or source file\n");
-        printf("%s\n", strerror(errno));
+        return 1;
     }
 
     return 0;
