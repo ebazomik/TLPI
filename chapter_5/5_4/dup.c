d8e4308740424429ecb8bb778ba9d4fbe2b70f17
ebazomik <kimosabe.mr@gmail.com>
2025-08-26 23:11:03 +0200

    fix control errors

--- chapter_5/5_4/dup.c
+++ chapter_5/5_4/dup.c
@@ -5,7 +5,7 @@
 int main(int argc, char ** argv){
 
     int opt, fd;
-    char * filepath;
+    char * filepath = NULL;
 
     if(argc < 2){
         printf("Usage: -f [filePath]\n");
@@ -21,13 +21,24 @@ int main(int argc, char ** argv){
         }
     }
 
+    if(filepath == NULL){
+        printf("Error: usage with -f [pathname] arg\n");
+        return 1;
+    }
+
 
     int flags = O_APPEND | O_RDWR;
     if((fd = open(filepath, flags)) == -1){
         printf("Error on open file: %s\n", filepath);
+        return 1;
     }
 
     int newFd = fcntl(fd, F_DUPFD);
+    if(newFd == -1){
+        printf("Error on duplication fd %d\n", fd);
+        close(fd);
+        return 1;
+    }
 
     printf("fd is %d\n", fd);
     printf("new fd is %d\n", newFd);
