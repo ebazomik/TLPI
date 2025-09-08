c2b7ae9ae357ea1707efe8e445ed3ad319bb05f6
ebazomik <kimosabe.mr@gmail.com>
2025-09-08 23:09:18 +0200

    fix shiftwidth

--- chapter_6/6_2/main.c
+++ chapter_6/6_2/main.c
@@ -5,34 +5,34 @@
 static jmp_buf env;
 
 static void f1(){
-				switch(setjmp(env)){
-								case 0: 
-												printf("setjmp case 0\n");
-												break;
-								case 1: 
-												printf("Return from longjmp case 1\n");
-												break;
-								default: 
-												printf("Return from case > 1\n");
-												break;
-				}
+		switch(setjmp(env)){
+				case 0: 
+						printf("setjmp case 0\n");
+						break;
+				case 1: 
+						printf("Return from longjmp case 1\n");
+						break;
+				default: 
+						printf("Return from case > 1\n");
+						break;
+		}
 }
 
 static void eating8Stack(){
-				int big_buff[8192];
-				for(int i = 0; i < sizeof(big_buff); i++){
-								big_buff[i] = 0xCC;
-				}
+		int big_buff[8192];
+		for(int i = 0; i < sizeof(big_buff); i++){
+				big_buff[i] = 0xCC;
+		}
 }
 
 int main(void){
 	
-				f1();
-				
-				//Overwrite 8KB of stack memory to ensure that there is no more data relating to f1.
-				eating8Stack();
+		f1();
+
+		//Overwrite 8KB of stack memory to ensure that there is no more data relating to f1.
+		eating8Stack();
 	
-				longjmp(env, 2);
-	
-				return 0;
+		longjmp(env, 2);
+
+		return 0;
 }
