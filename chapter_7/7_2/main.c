e5b399f7037b99fcdd5a8b2f0d6dcc5b6f6e1811
ebazomik <kimosabe.mr@gmail.com>
2025-09-28 22:36:12 +0200

    added initial comment

--- chapter_7/7_2/main.c
+++ chapter_7/7_2/main.c
@@ -1,6 +1,15 @@
-// todo (study)
-// - add new POOL_SIZE if all poll is filled
-// - align data before allocation (check info)
+/*	
+ *	Copyright (C) 2025 ebazomik
+ *
+ *	Simple implementation of linear memory allocation for study purpose.
+ *
+ *	This implementation creates a main pool of 8129 bytes and focuses on
+ *	using virtual memory to manage allocation without calling the sbrk
+ *	system call for each allocation.
+ *  
+ *	Licence: MIT
+ */
+
 
 #include <unistd.h>
 #include <stdio.h>
@@ -8,7 +17,7 @@
 #include <errno.h>
 #include <stdint.h>
 
-#define POOL_SIZE 1024
+#define POOL_SIZE 8192
 
 struct ek_chunk{
 		int free;
@@ -38,90 +47,106 @@ static void * build_chunk(
 }
 
 void * ek_alloc(size_t size){
+		
+		if(size == 0) return NULL;
+		size_t real_size = sizeof(struct ek_chunk) + size;
+		
+		if(real_size > POOL_SIZE){
+				
+				printf("Virtual memory limit reached (%d) when attempting to allocate %d\n", POOL_SIZE, real_size);
+				return NULL;
+		
+		}
 
 		if(prog_brk == NULL){
+
 				prog_brk = sbrk(POOL_SIZE);
 				initial_brk = prog_brk;
 				start_chunk = (struct ek_chunk *) prog_brk;
 				build_chunk(start_chunk, size, (void *) (start_chunk + 1), NULL);
+				
 				return start_chunk->my_p;
+		
 		}
 
 		struct ek_chunk * current = start_chunk;
 
 		while(current->next != NULL){
-				current = current->next;
+
 				if(current->free) break;
+				current = current->next;
+
 		}
-	
 
-		char* current_end = 
-				(char *)current + sizeof(struct ek_chunk) + current->size;
-
-		char* end_of_chunk = 
-				(char *)initial_brk + POOL_SIZE;
-
+		size_t current_chunk_space = sizeof(struct ek_chunk) + current->size;
+		char* current_end = (char *)current + current_chunk_space;
+		char* end_pool = (char *)initial_brk + POOL_SIZE;
 		size_t space_remaining = 0;
 
-		if(!current->free)
-				space_remaining = end_of_chunk - current_end;
+		if(!current->free) {
 
-		size_t space_needed = sizeof(struct ek_chunk) + size;
+				space_remaining = end_pool - current_end;
 
-		int write_to_current = 
-				(space_needed <= sizeof(struct ek_chunk) + current->size) &&
-				current->free;
-
-		if(write_to_current){
-				build_chunk(current, size, (void *) (current + 1), NO_NEXT);
-				return current->my_p;
 		}
 
-		if(space_remaining >= space_needed){
+		size_t space_needed = sizeof(struct ek_chunk) + size;
+		int write_to_current = (space_needed <= current_chunk_space) && current->free;
+
+		if(write_to_current){
+
+				build_chunk(current, size, (void *) (current + 1), NO_NEXT);
+
+				return current->my_p;
+
+		}
+
+		if (space_remaining >= space_needed){
+
 				struct ek_chunk * new = (struct ek_chunk *) current_end;
 				build_chunk(new, size,(void*) (new + 1), NULL);
 				current->next = new;
+
 				return new->my_p;
+
 		} else {
-				// increment brk
-				printf("POOL_SIZE full, space remaining: %d\n", space_remaining);
+
+				printf("Can't allocate %d in remaing vistual space %d\n", space_needed, space_remaining);
+
 				return NULL;
 		}
-
-
 }
 
+
 int ek_free(void * p){
-		struct ek_chunk * temp =
-				(struct ek_chunk *) ((char *) p - sizeof(struct ek_chunk));
+		
+		if(p == NULL) return 1;
+
+		struct ek_chunk * temp = (struct ek_chunk *) ((char *) p - sizeof(struct ek_chunk));
 		temp->free = 1;
 		temp->my_p = NULL;
+
 		return 0;
 }
 
 
-
-
 int main(void){
 		
-		int * p = ek_alloc(sizeof(int));
-		*p = 50;
-		printf("p:\t%p -> %d\n", p, *p);
+		int * p = ek_alloc(4096);
+		if(p != NULL)
+				*p = 200;
+				printf("p:\t%p -> %d\n", p, *p);
 
-		int * p2 = ek_alloc(sizeof(int));
-		*p2 = 100;
-		printf("p2:\t%p -> %d\n", p2, *p2);
+		char * p2 = ek_alloc(8192);
+		if(p2 != NULL){
+				strcpy(p2, "Hi, this is message allocated to p2");
+				printf("p2:\t%p -> %s\n", p2, p2);
+		}
 
 		int * p3 = ek_alloc(sizeof(int));
-		*p3 = 150;
-		printf("p3:\t%p -> %d\n", p3, *p3);
-
-		ek_free(p2);
-
-		int * p4 = ek_alloc(sizeof(int));
-		*p4 = 200;
-		printf("p4:\t%p -> %d\n", p4, *p4);
-
+		if(p3 != NULL){
+				*p3 = 300;
+				printf("p4:\t%p -> %d\n", p3, *p3);
+		}
 
 		return 0;
 }
